47aecf0a71d2456f63c49908d7261877b2a1bd5a
Raj Singh <152046820+Raj-Singh-3@users.noreply.github.com>
2025-09-15 23:32:41 +0530

    Time: 6 ms (72.15%), Space: 45.1 MB (23.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,18 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // code here
-        int lastIndex[] = new int[128];
-        // why 128 beacuse there are 128 charcters
-        // so 128 space will be considered as constant because everytime whatever the String,
-        // the Space will be 128.
-        Arrays.fill(lastIndex,-1);
-        int maxLen = 0;
-        int start = 0;
-        for(int i = 0;i<s.length();i++)
+        HashMap<Character,Integer> ht = new HashMap<>();
+        int l = 0, r = 0, maxLen = 0, n = s.length();
+        while(r<n)
         {
-            char ch = s.charAt(i);
-            if(lastIndex[ch] >= start)
+            char ch = s.charAt(r);
+            if(ht.containsKey(ch))
             {
-                start = lastIndex[ch]+1;
+                l = Math.max(l, ht.get(ch) + 1);
+                ht.remove(ch);
             }
-            lastIndex[ch] = i;
-            maxLen = Math.max(maxLen,i-start+1);
+            ht.put(ch,r);
+            maxLen = Math.max(maxLen, r-l+1);
+            r++;
         }
         return maxLen;
     }
