b3c62b96398886dde5769e239a02052358a681ec
Raj Laxman Singh <152046820+Raj-Singh-3@users.noreply.github.com>
2026-06-19 17:51:33 +0530

    Time: 0 ms (100%), Space: 43.2 MB (18.48%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int arr[] = new int[gain.length+1];
+        arr[0] = 0;
+        for(int i = 1;i<=gain.length;i++)
+        {
+            arr[i] = arr[i-1]+gain[i-1];
+        }
+        int max = arr[0];
+        for(int i = 0;i<arr.length;i++)
+        {
+            if(max<arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
